3506ae8aacdb991c61291542bc5faa97290b5933
lorenzopugina <lorenzopugina10@gmail.com>
2025-09-18 16:40:05 -0300

    Comentarios

--- Script/global.js
+++ Script/global.js
@@ -1,13 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // cria as nuvens automaticamente
   const nuvens = document.createElement("div");
-  nuvens.className = "nuvens";
+  nuvens.className = "nuvem";
   nuvens.innerHTML = `
-    <div class="nuvem">
       <img src="Imagens/nuvem.png" class="nuvem1">
       <img src="Imagens/nuvem.png" class="nuvem2">
       <img src="Imagens/nuvem.png" class="nuvem3">
-    </div>
   `;
   document.body.appendChild(nuvens);
 
@@ -19,7 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const elapsed = (Date.now() - startTime) / 1000; // segundos
 
   // aplica o deslocamento na animação de cada nuvem
-  document.querySelectorAll(".nuvens img").forEach(el => {
+  document.querySelectorAll(".nuvem img").forEach(el => {
     const dur = parseFloat(getComputedStyle(el).animationDuration); // duração em segundos
     const offset = elapsed % dur;
     el.style.animationDelay = `-${offset}s`;
